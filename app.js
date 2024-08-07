762a822135585ee9b5ab5df235d986331a759fb7
Frank <zi.h.yin@mail.mcgill.ca>
2024-08-07 04:59:09 -0400

    Created on 2024-08-07 at 04:59:09

---
+++ app.js
@@ -0,0 +1,63 @@
+let lineOrder = '';
+let measures = [];
+
+document.getElementById('lineOrder').addEventListener('change', function() {
+    lineOrder = this.value;
+});
+
+function addMeasure() {
+    const beatCount = document.getElementById('beatCount').value;
+    if (beatCount) {
+        measures.push({ beatCount });
+        document.getElementById('beatCount').value = ''; // Reset input
+        updateXMLPreview(); // Update XML preview
+    }
+}
+
+function updateXMLPreview() {
+    const xmlContent = renderXML();
+    document.getElementById('xmlPreview').textContent = xmlContent;
+}
+
+function renderXML() {
+    return `
+<mei xmlns="http://www.music-encoding.org/ns/mei">
+    <meiHead>
+        <fileDesc>
+            <titleStmt>
+                <title type="main">Stravinsky</title>
+            </titleStmt>
+            <pubStmt/>
+        </fileDesc>
+    </meiHead>
+    <music>
+        <body>
+            <mdiv>
+                <score>
+                    <scoreDef line-order="${lineOrder}">
+                        <measureGrp>
+                            ${measures.map((m, index) => `<measureDef n="${index + 1}" bcount="${m.beatCount}"/>`).join('')}
+                        </measureGrp>
+                    </scoreDef>
+                    <section>
+                        <!-- XML based on measures and beats can be constructed here -->
+                    </section>
+                </score>
+            </mdiv>
+        </body>
+    </music>
+</mei>`;
+}
+
+function downloadXML() {
+    const xmlBlob = new Blob([renderXML()], { type: 'application/xml' });
+    const url = URL.createObjectURL(xmlBlob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = "MEI_Stravinsky.xml";
+    link.click();
+    URL.revokeObjectURL(url); // Clean up
+}
+
+// Initialize XML preview
+updateXMLPreview();
