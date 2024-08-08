d0b4fb1712c329a2c2c931ca1e80c0ff94140873
Frank <zi.h.yin@mail.mcgill.ca>
2024-08-08 16:34:46 -0400

    working with pretty much all

--- app.js
+++ app.js
@@ -16,12 +16,38 @@ document.getElementById('lineOrder').addEventListener('change', function() {
 });
 
 function addMeasure() {
-    const beatCount = document.getElementById('beatCount').value;
-    if (beatCount) {
-        measures.push({ beatCount });
-        document.getElementById('beatCount').value = ''; // Reset input
-        updateXMLPreview(); // Update XML preview
-    }
+  const beatCount = document.getElementById('beatCount').value;
+  if (beatCount) {
+      measures.push({ beatCount, braces: [] });
+      document.getElementById('beatCount').value = ''; // Reset input
+      updateXMLPreview(); // Update XML preview
+  }
+}
+
+
+function addCurvedBrace() {
+  const starID1 = document.getElementById('starPair1').value;
+  const starID2 = document.getElementById('starPair2').value;
+  // Find measure and beat index for the stars, validate if they belong to the same beat
+  const star1 = findStar(starID1);
+  const star2 = findStar(starID2);
+
+  if (star1 && star2 && star1.measureIndex === star2.measureIndex && star1.beatIndex === star2.beatIndex) {
+      measures[star1.measureIndex].braces.push({ star1: starID1, star2: starID2, bracketType: "standard" });
+      updateXMLPreview();
+  } else {
+      alert("Stars must belong to the same beat.");
+  }
+}
+
+function findStar(starID) {
+  let [measureIndex, beatIndex, line] = starID.split(' ').map(Number);
+  measureIndex--; // Adjust index as array is zero-based
+  beatIndex--;
+  if (measures[measureIndex] && measures[measureIndex].beatCount >= beatIndex) {
+      return { measureIndex, beatIndex, line };
+  }
+  return null;
 }
 
 function updateXMLPreview() {
@@ -29,7 +55,8 @@ function updateXMLPreview() {
 }
 
 function renderXML() {
-    return `<mei xmlns="http://www.music-encoding.org/ns/mei">
+  const lines = lineOrder.split(' ').map(n => `                <star id="${n}" class="" ifStar="false" ifBracket="false"/>`).join('\n');
+  return `<mei xmlns="http://www.music-encoding.org/ns/mei">
   <meiHead>
     <fileDesc>
       <titleStmt>
@@ -50,11 +77,14 @@ ${measures.map((m, index) => `              <measureDef n="${index + 1}" bcount=
           <section>
 ${measures.map((measure, mIndex) => `            <measure n="${mIndex + 1}">
 ${Array.from({length: measure.beatCount}, (_, bIndex) => `              <beat n="${bIndex + 1}">
-${lineOrder.split(' ').map(line => `                <star id="${mIndex + 1} ${bIndex + 1} ${line}" class="" ifStar="false" ifBracket="false"/>`).join('\n')}
+${lines}
               </beat>
               <setLabel beat="${bIndex + 1}" place="above"></setLabel>
-              <curvedBrace beat="${bIndex + 1}" place="right"></curvedBrace>`).join('\n')}
-            </measure>`).join('\n')}
+              <!-- optional vertical braces -->
+${renderVerticalBraces(measure.braces, mIndex, bIndex)}`
+              ).join('\n')}
+            </measure>`
+            ).join('\n')}
           </section>
         </score>
       </mdiv>
@@ -62,6 +92,18 @@ ${lineOrder.split(' ').map(line => `                <star id="${mIndex + 1} ${bI
   </music>
 </mei>`;
 }
+
+
+function renderHorizontalBraces(braces, measureIndex, beatIndex) {//unused yet
+  return braces.filter(brace => brace.star1.startsWith(`${measureIndex + 1} ${beatIndex + 1}`) && brace.star2.startsWith(`${measureIndex + 1} ${beatIndex + 1}`))
+      .map(brace => `              <horizontalBrace star1="${brace.star1}" star2="${brace.star2}" bracketType="${brace.bracketType}"></curvedBrace>`).join('\n');
+}
+
+function renderVerticalBraces(braces, measureIndex, beatIndex) {
+  return braces.filter(brace => brace.star1.startsWith(`${measureIndex + 1} ${beatIndex + 1}`) && brace.star2.startsWith(`${measureIndex + 1} ${beatIndex + 1}`))
+      .map(brace => `              <curvedBrace star1="${brace.star1}" star2="${brace.star2}" bracketType="${brace.bracketType}"></curvedBrace>`).join('\n');
+}
+
 
 function sanitizeFilename(title) {
     return title.replace(/[<>:"/\\|?*]+/g, '').replace(/[\s]+/g, '_'); // Remove problematic characters and replace spaces with underscores
